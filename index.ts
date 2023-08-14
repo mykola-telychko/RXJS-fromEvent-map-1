d38359891c2013ffc4fcf62e1b02705d884510c5
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-14 21:06:21 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import { fromEvent } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/creation/fromevent
+// Example 1: Observable from mouse clicks
+
 //create observable that emits click events
 const source = fromEvent(document, 'click');
 //map to string with given event timestamp
 const example = source.pipe(map((event) => `Click time: ${event.timeStamp}`));
-//output (example): 'Event time: 7276.390000000001'
+
 const subscribe = example.subscribe((val) => console.log(val));
